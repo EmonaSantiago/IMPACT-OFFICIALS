3050126b6c66ac0221e7e91fa033e6a1ed19341c
Emona Santiago <emonasantiago1207@gmail.com>
2024-05-29 20:17:55 +0200

    Workshops page

--- src/routes/WORKSHOPS/+page.svelte
+++ src/routes/WORKSHOPS/+page.svelte
@@ -2,18 +2,18 @@
 <body>
 <div class="container">
     <div class="item">
-      <img src="/src/static/aftermovie.png" alt="Image 1">
-      <h2>Title 1</h2>
-      <p>Description for Image 1</p>
-      <a href="/WORKSHOP1">Read More</a>
+      <img src="/src/static/workshop-marsam.png" alt="Image 1">
+      <h2>PAINT & VIBE</h2>
+      <p>Marsam goes beyond painting</p>
+      <a href="https://www.instagram.com/p/C7Cv4qrslnP/?img_index=1">Read More</a>
     </div>
     <div class="item">
-        <img src="/src/static/aftermovie.png" alt="Image 1">
-        <h2>Title 1</h2>
-        <p>Description for Image 1</p>
-        <a href="/WORKSHOP1">Read More</a>
+        <img src="/src/static/workshop-marica.png" alt="Image 1">
+        <h2>CONNECT WITH ME</h2>
+        <p>CONNECT with MARICA </p>
+        <a href="https://www.instagram.com/p/C69AuQSsviv/?img_index=1">Read More</a>
       </div>
-      
+    
     <!-- Add more .item divs for additional images -->
   </div>
 </body>
@@ -23,6 +23,19 @@
         margin: 1em;
     }
 
+    h2 {
+      color: var(--IMPACT-YELLOW);
+    }
+
+    p {
+      color: white;
+      font-weight: 100;
+      text-align: center;
+    }
+
+    a {
+      text-decoration: underline;
+    }
     body {
     margin: 0;
     padding: 0;
